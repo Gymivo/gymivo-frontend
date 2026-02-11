3a02b302ce8502f54e6a658622be6ef79c304592
ALI-HUP <ali.sheikhbahaie84@gmail.com>
2026-02-11 20:06:44 +0330

    add new pages + corrections

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,17 +1,17 @@
-import Button from "./Button";
+import Link from "next/link";
 import Image from "next/image";
-import gymivoIcon from "@/public/svg/header/gymivo.png";
-import WhatsappIcon from "@/public/svg/social/Whatsapp.svg";
-import TelegramIcon from "@/public/svg/social/Telegram.svg";
-import GmailIcon from "@/public/svg/social/Gmail.svg";
-import InstagramIcon from "@/public/svg/social/Instagram.svg";
-import FrameIcon from "@/public/svg/social/picture-frame.svg";
+import gymivoIcon from "@/public/svg/footer/gymivo.svg";
+import WhatsappIcon from "@/public/svg/footer/Whatsapp.svg";
+import TelegramIcon from "@/public/svg/footer/Telegram.svg";
+import GmailIcon from "@/public/svg/footer/Gmail.svg";
+import InstagramIcon from "@/public/svg/footer/Instagram.svg";
+import FrameIcon from "@/public/svg/footer/picture-frame.svg";
 
 export default function Footer() {
   return (
     <div className="bg-neutral-darker">
       <div className="p-5">
-        <Image src={gymivoIcon} alt="gimivo" width={100} height={24} />
+        <Image src={gymivoIcon} alt="gimivo" width={120} height={50} />
       </div>
 
       <div className="flex justify-between">
@@ -19,21 +19,21 @@ export default function Footer() {
           <h2 className="py-4">
             صفحات <span className="text-primary-300">جیمیوو</span>
           </h2>
-          <p>درباره ما</p>
-          <p>ارتباط با ما</p>
-          <p>قوانین و مقررات</p>
-          <p>سوالات متداول</p>
-          <p>همکاری با جیمیوو</p>
+          <Link className="hover:underline" href="/about"><p>درباره ما</p></Link>
+          <Link className="hover:underline" href="/contact"><p>ارتباط با ما</p></Link>
+          <Link className="hover:underline" href="/terms"><p>قوانین و مقررات</p></Link>
+          <Link className="hover:underline" href="/faq"><p>سوالات متداول</p></Link>
+          <Link className="hover:underline" href="/cooperate"><p>همکاری با جیمیوو</p></Link>
         </div>
 
         <div className="p-5 flex flex-col">
           <h2 className="py-4">
             خدمات <span className="text-primary-300">جیمیوو</span>
           </h2>
-          <p>برنامه های من</p>
-          <p>جستجوی حرکات</p>
-          <p>ساخت برنامه شخصی</p>
-          <p>جستجوی مربیان</p>
+          <Link className="hover:underline" href="/myprograms"><p>برنامه های من</p></Link>
+          <Link className="hover:underline" href="/movements"><p>جستجوی حرکات</p></Link>
+          <Link className="hover:underline" href="/createprogram"><p>ساخت برنامه شخصی</p></Link>
+          <Link className="hover:underline" href="/coaches"><p>جستجوی مربیان</p></Link>
         </div>
       </div>
 
@@ -50,9 +50,9 @@ export default function Footer() {
       <div className="p-5">
         <h2>نمادها</h2>
         <div className="py-5 flex justify-between">
-          <Image alt="frame1" src={FrameIcon} />
-          <Image alt="frame2" src={FrameIcon} />
-          <Image alt="frame3" src={FrameIcon} />
+          <Image alt="frame1" src={FrameIcon} width={110} height={110}/>
+          <Image alt="frame2" src={FrameIcon} width={110} height={110}/>
+          <Image alt="frame3" src={FrameIcon} width={110} height={110}/>
         </div>
       </div>
 
